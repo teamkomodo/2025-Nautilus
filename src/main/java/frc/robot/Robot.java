fdd34346022702ab7ac789047ab2a0ff5fb2f7d6
boraayhan <borrayhan3737@gmail.com>
2025-02-27 18:22:05 -0700

    garbage shuffleboard camera code test

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -6,9 +6,12 @@ package frc.robot;
 
 import org.littletonrobotics.urcl.URCL;
 
+import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.cscore.HttpCamera;
 import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
@@ -29,13 +32,17 @@ public class Robot extends TimedRobot {
     */
     @Override
     public void robotInit() {
-        // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
+        
+        // Instantiate our RobotConztainer.  This will perform all our button bindings, and put our
         // autonomous chooser on the dashboard.
         m_robotContainer = new RobotContainer();
         
         DataLogManager.start();
         DriverStation.startDataLog(DataLogManager.getLog(), true);
         URCL.start();
+        HttpCamera httpCamera = new HttpCamera("CoprocessorCamera", "http://frcvision.local:1181/stream.mjpg");
+        CameraServer.getVideo(httpCamera);
+        Shuffleboard.getTab("Tab").add(httpCamera);
     }
     
     /**
